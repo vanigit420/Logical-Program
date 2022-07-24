fb61ad040d7c0ee186397a9569581b54c1e03fdd
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-24 13:31:36 +0530

    finding the perfect number within given a range

---
+++ Logical-Program/Logical-Program/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LogicalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, num;
+
+            Console.WriteLine("Enter the number of Elements: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n1 + " " + n2 + " ");
+
+            for (int i = 2; i < num; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
